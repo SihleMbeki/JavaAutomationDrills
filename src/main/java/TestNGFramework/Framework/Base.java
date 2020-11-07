1f84ea53ed28e879c4e6e9abe46fb04c1bca3652
Masakhane Matanzima <sihle.matanzima@gmail.com>
2020-11-07 21:50:01 +0200

    Test Data Parameterization

--- src/main/java/TestNGFramework/Framework/Base.java
+++ src/main/java/TestNGFramework/Framework/Base.java
@@ -5,50 +5,58 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
+
+import Utilities.ExcelReader;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
 public class Base {
+	protected String excelFileName;
+	protected ExcelReader excel;
+	String directory;
 
-  @BeforeMethod
-  public void beforeMethod() {
-	  System.out.println("Before Method ");
-  }
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method ");
+	}
 
-  @AfterMethod
-  public void afterMethod() {
-	  System.out.println("After Method ");
-  }
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method ");
+	}
 
-  @BeforeClass
-  public void beforeClass() {
-	  System.out.println("Before Class ");
-  }
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before Class ");
+	}
 
-  @AfterClass
-  public void afterClass() {
-	  System.out.println("After class ");
-  }
+	@AfterClass
+	public void afterClass() {
+		System.out.println("After class ");
+	}
 
-  @BeforeTest
-  public void beforeTest() {
-	  System.out.println("Before Test ");
-  }
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test ");
+	}
 
-  @AfterTest
-  public void afterTest() {
-	  System.out.println("After test ");
-  }
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After test ");
+	}
 
-  @BeforeSuite
-  public void beforeSuite() {
-	  System.out.println("Before Suite ");
-  }
+	@BeforeSuite
+	public void beforeSuite() {
+		directory=System.getProperty("user.dir");
+		excelFileName=directory+"\\src\\test\\resources\\testdata.xlsx";
+		excel=new ExcelReader(excelFileName);
+	}
 
-  @AfterSuite
-  public void afterSuite() {
-	  System.out.println("After Suite ");
-  }
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After Suite ");
+	}
 
 }
